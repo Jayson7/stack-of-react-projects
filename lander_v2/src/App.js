be6478c6016d7aeac00a4f78c71964a0645e1d2f
jayson7 <lexxiijoo70gmail.com>
2021-09-18 23:44:24 +0100

    reset

---
+++ lander_v2/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text">
+        <h1>
+          Hello <span> World </span>
+        </h1>
+        <h5> I 'm An Expert at Building Dynamic Web Apps</h5>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.Necessitatibus
+          vitae, accusamus aperiam suscipit at amet unde nobis
+          ullam!Exercitationem laborum voluptate dignissimos officiis vero cum
+          amet quam iusto molestias officia.
+        </p>
+        <div className="buttons">
+          <button> Explore </button> <button>View More</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ lander_v2/src/components/navbar/nav.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./nav.css";
+export default function Nav() {
+  return (
+    <div>
+      <div className="navbar">
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
